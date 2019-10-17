41a1f8cd1d27a9fdce52ec4afd6ebb1bb2019122
Sajal Gupta <53595953+sajal243@users.noreply.github.com>
2019-10-17 15:07:56 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -1,1 +1,83 @@
-
+const myQuestions = [
+    {
+      question: "What is Half Wave Rectifier?",
+      answers: {
+        a: "half wave rectifier converts only half of the AC cycle to Uni-direction.",
+        b: "half wave rectifier converts only half of the AC cycle to bi-direction.",
+        c: "both a & b",
+        d: "none of the above"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is the efficiency of a rectifier?",
+      answers: {
+        a: "The ratio of AC output power to the applied AC input power is known as the efficiency of a rectifier",
+        b: "The ratio of DC output power to the applied AC output power is known as the efficiency of a rectifier.",
+        c: "The ratio of DC output power to the applied AC input power is known as the efficiency of a rectifier.",
+        d: "none of the above"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the maximum efficiency (η) of a half wave rectifier?",
+      answers: {
+        a: "90.3%",
+        b: "93.5%",
+        c: "47.3%",
+        d: "40.6%"
+      },
+      correctAnswer: "d"
+    },
+     {
+      question: "What is form factor?",
+      answers: {
+        a: "The ratio of maximum value to the RMS value.",
+        b: "It is the ratio of the RMS value to the Average value.",
+        c: "The ratio of RMS value to the maximum value.",
+        d: "The ratio of average value to the RMS value."
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "What is peak factor?",
+      answers: {
+        a: "The ratio of maximum value to the RMS value.",
+        b: "It is the ratio of the RMS value to the Average value.",
+        c: "The ratio of RMS value to the maximum value.",
+        d: "The ratio of average value to the RMS value."
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is the TUF of a half wave rectifier",
+      answers: {
+        a: "0.1047",
+        b: "1.2222",
+        c: "0.0069",
+        d: "0.2865"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "What is the importance of PIV voltage of a Diode in A Rectifier Circuit",
+      answers: {
+        a: "PIV (Peak Inverse Voltage) is the maximum possible voltage that a diode can withstand in the forward bias.",
+        b: "PIV (Peak Inverse Voltage) is the maximum possible voltage that a diode can withstand in the reverse bias.",
+        c: "PIV (Peak Inverse Voltage) is the minimum optimum voltage that a diode can withstand in the reverse bias",
+        d: "none of the above"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Ripple voltage in a rectifier output is the amount of ……… content present in the output DC, it causes periodic pulsating of the DC voltage which is derived from the AC source.",
+      answers: {
+        a: "AC",
+        b: "DC",
+        c: "both a & b",
+        d: "none of the above"
+      },
+      correctAnswer: "a"
+    }
+    
+  ];
